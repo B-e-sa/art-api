b3d840a1a6ab0a82080630dccbdd9c40f45a271a
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-11-17 15:12:04 -0300

    add cors depedency

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 3000
 
 const art = require('./art')
+
+app.use(cors())
 
 app.get('/art', (req, res) => {
     res.json(art)
